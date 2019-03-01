8fb8d732ddc6a4e9ab0b78a869c9d3150f455f05
Lightfaith <vitezslav.grygar@gmail.com>
2019-03-01 14:47:00 +0100

    CTR bitflipping (cryptopals 4.26)

---
+++ ctr/cryptopals_26_decrypt.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import subprocess
+import base64 
+
+try:
+    data = sys.argv[1]
+except:
+    print('[-] Usage: %s <base64>' % sys.argv[0])
+    sys.exit(1)
+
+data = base64.b64decode(data)
+datafile = '/tmp/cryptopals_26_%d_decryptdata' % os.getpid()
+keyfile = '/tmp/cryptopals_26_key'
+with open(datafile, 'wb') as f:
+    f.write(data)
+with open(keyfile, 'wb') as f:
+    f.write(b'YELLOW SUBMARINE')
+
+#sys.stderr.buffer.write(data)
+p = subprocess.Popen('./langdon --decrypt aes-ctr {0} {1} 1337; rm {0}'.format(
+    datafile, keyfile),
+                     shell=True,
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE)
+(out, err) = p.communicate()
+sys.stdout.buffer.write(out)
+sys.stderr.buffer.write(err)
+
--- ctr/cryptopals_26_encrypt.py
+++ ctr/cryptopals_26_encrypt.py
@@ -13,22 +13,22 @@ except:
 
 """
 accepting email, sanitizing,
-generating email,uid,role and encrypting with AES CBC
+generating email,uid,role and encrypting with AES CTR
 """
 data = (b'comment1=cooking%%20MCs;userdata=%s;comment2=%%20like%%20a%%20pound%%20of%%20bacon'
         % data.replace(b'=', b'--').replace(b';', b',.'))
-datafile = '/tmp/cryptopals_16_%d_encryptdata' % os.getpid()
-keyfile = '/tmp/cryptopals_16_key'
+datafile = '/tmp/cryptopals_26_%d_encryptdata' % os.getpid()
+keyfile = '/tmp/cryptopals_26_key'
 with open(datafile, 'wb') as f:
     f.write(data)
 with open(keyfile, 'wb') as f:
     f.write(b'YELLOW SUBMARINE')
 
-p = subprocess.Popen('./langdon --encrypt aes-cbc {0} {1} 0000000000000000; rm {0}'.format( 
+p = subprocess.Popen('./langdon --encrypt aes-ctr {0} {1} 1337; rm {0}'.format( 
     datafile, keyfile),
                      shell=True,
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE)
 (out, err) = p.communicate()
 sys.stdout.buffer.write(out)
-#sys.stderr.buffer.write(data)
+sys.stderr.buffer.write(data)
